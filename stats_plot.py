2ebf42f0b5d21b63b3f63a73de266961f9722ebb
lwangreen <wanluming@msn.com>
2019-01-31 12:15:09 +0800

    Update stats_plot.py

--- stats_plot.py
+++ stats_plot.py
@@ -6,20 +6,41 @@ INPUT_DIRECTORY = os.getcwd()+"\\"+"2019-01-23 06-12-58_plot_stats"
 # OUTPUT_DIRECTORY = "2019-01-23 06-12-58_plot_stats"
 X_FILLED = False
 x = []
+line_names = []
 stats_files = os.listdir(INPUT_DIRECTORY)
 print(stats_files)
 
 for file in stats_files:
     if file[-4:] == ".csv":
+        x_cols = []
+        y_label = file[-4:]
+
         f = open(INPUT_DIRECTORY+"\\"+file)
-        f.readline()
+        if not line_names:
+            line_names = f.readline().split(", ")[1:]
+        else:
+            line = f.readline()
         line = f.readline()
         while line:
             line = line.split(", ")
             if not X_FILLED:
                 x.append(line[0])
-            
+
+            for index in range(1, len(line)):
+                if len(x_cols) <= (index - 1):
+                    x_cols.append([float(line[index])])
+                else:
+                    x_cols[index-1].append(float(line[index]))
+            line = f.readline()
         X_FILLED = True
+        f.close()
+        for x_col in x_cols:
+            print(x_col)
+        print("---------------------------------------------------")
+
+
+print(line_names)
+
 # x = ["0-600", "600-1200", "1200-1800", "1800-2400"]
 # y = [1,2,3,4]
 # f= plt.figure()
